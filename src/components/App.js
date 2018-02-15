3a9ca0315a0e7aae406f86bbe73ce82d9b073656
Matthew Holman <mateoholman@gmail.com>
2018-02-14 19:59:33 -0600

    Change folder structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Use the Star Wars API</p>
+      </div>
+    );
+  }
+}
+
+export default App;
