4c540f45c9d48774ca9947acfba183ea7ef29698
Matthew Holman <mateoholman@gmail.com>
2018-04-06 22:19:18 -0500

    Add App container

--- src/index.js
+++ src/index.js
@@ -7,5 +7,11 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
