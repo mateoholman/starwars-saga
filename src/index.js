7e959bf3159a4a9a6b37086846fa4740b45029a7
Matthew Holman <mateoholman@gmail.com>
2018-04-06 22:14:07 -0500

    Added redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './store/index.js'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
