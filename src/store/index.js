ba06e2d56a7bef43b07b4e622dacc72f6d49b800
Matthew Holman <mateoholman@gmail.com>
2018-04-11 21:56:06 -0500

    Added devtools middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga'
 
 import reducer from '../reducers'
@@ -8,7 +9,7 @@ const sagaMiddleware = createSagaMiddleware()
 
 export const store = createStore(
   reducer,
-  applyMiddleware(sagaMiddleware)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
 
 sagaMiddleware.run(mySaga)
