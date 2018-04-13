1544e255a558d4ba07d4aa8fd7d3b869d5d2df3c
Matthew Holman <mateoholman@gmail.com>
2018-04-12 22:04:44 -0500

    New action

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,10 @@ export const fetchStarWarsRequest = () => ({
     type: TYPES.FETCH_STAR_WARS_REQUEST
 })
 
+export const fetchStarshipRequest = () => ({
+  type: TYPES.FETCH_STARSHIP_REQUEST
+})
+
 export function* fetchPerson(action) {
    try {
       const person = yield call(api, 'https://swapi.co/api/people/');
@@ -15,4 +19,13 @@ export function* fetchPerson(action) {
    } catch (e) {
        console.log(e)
    }
+}
+
+export function* fetchStarships(action) {
+  try {
+    const starship = yield call(api, 'https://swapi.co/api/starships/');
+    yield put({ type: TYPES.FETCH_STAR_WARS_SUCCESS, data: starship.results });
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux'
 import * as TYPES from '../types'
 
 const initialState = {
-    people: []
+    people: [],
+    starships: []
 }
 
 const handleStarWarsFetchSuccess = (state, action) => {
@@ -12,9 +13,17 @@ const handleStarWarsFetchSuccess = (state, action) => {
     }
 }
 
+const handleStarshipFetchSuccess = (state, action) => {
+  return {
+    ...state,
+    starships: action.data
+  }
+}
+
 const starWars = (state = initialState, action) => {
     const handlers = {
-        [TYPES.FETCH_STAR_WARS_SUCCESS]: handleStarWarsFetchSuccess
+        [TYPES.FETCH_STAR_WARS_SUCCESS]: handleStarWarsFetchSuccess,
+        [TYPES.FETCH_STARSHIP_REQUEST]: handleStarshipFetchSuccess
     }
     return handlers[action.type]
         ? handlers[action.type](state, action)
